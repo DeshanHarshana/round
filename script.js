2c2a97fbbbc9b9e28f543b47f486e8096cfe7564
DeshanHarshana <nawarathnadeshan@gmail.com>
2022-05-01 19:15:50 +0530

    change

--- script.js
+++ script.js
@@ -10,16 +10,17 @@ function myFunction() {
         var y = Math.floor(Math.random() * 256);
         var z = Math.floor(Math.random() * 256);
         var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-        
+        var borderRadius=Math.floor(Math.random() * 50).toString()+"%"
         newStringx=randomNumbex.toString()+"%"
         newStringy=randomNumbey.toString()+"%"
         
         document.getElementById('1').style.left=newStringx;
         document.getElementById('1').style.top=newStringy;
-        document.getElementById('1').style.transitionDuration="2s"
+        document.getElementById('1').style.transitionDuration="1s"
         document.getElementById('1').style.backgroundColor=bgColor
+        document.getElementById('1').style.borderRadius=borderRadius
         
-    },2000)
+    },1000)
     
     
 
